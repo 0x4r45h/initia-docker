8e0280b810d5073055bb9c511a8fd399c4d01d46
0x4r45h <0x4r45h@gmail.com>
2024-05-31 03:51:51 +0330

    Improve init script

--- init.sh
+++ init.sh
@@ -8,10 +8,16 @@ initiad init $MONIKER --chain-id $CHAIN_ID 2>&1 | cat - >> init_output.txt
 wget https://initia.s3.ap-southeast-1.amazonaws.com/initiation-1/genesis.json -O $HOME/.initia/config/genesis.json
 
 # setting minimum-gas-prices = "0.15uinit,0.01uusdc"
-sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.15uinit,0.01uusdc\"|" $HOME/.initia/config/app.toml
-#change listen address
-sed -i "s#address = \"localhost:9090\"#address = \"0.0.0.0:9090\"#g" $HOME/.initia/config/app.toml
+initiad config set app minimum-gas-prices "0.15uinit,0.01uusdc"
+
+#change api listen address
+initiad config set app api.address "0.0.0.0:9090"
 
 #enable oracle
-sed -i '/^\[oracle\]/,/^\[/ s/^\(enabled\s*=\s*\)"false"/\1"true"/' $HOME/.initia/config/app.toml
-sed -i '/^\[oracle\]/,/^\[/ s/^\(oracle_address\s*=\s*\)""/\1"oracle:8080"/' $HOME/.initia/config/app.toml
+initiad config set app oracle.enabled "true"
+initiad config set app oracle.oracle_address "oracle:8080"
+
+# Set config.toml
+# disable indexer
+sed -i "s/^indexer *=.*/indexer = \"null\"/" $HOME/.initia/config/config.toml
+
